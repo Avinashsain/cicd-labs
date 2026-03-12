ad4d88cd16027902c617e0e00956e0c07a4a42a6
Avinashsain <avinashsain65@gmail.com>
2026-03-12 23:45:12 +0530

    deployment

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 items = [
     {"id": 1, "name": "Build pipeline on GIT", "done": True},
     {"id": 2, "name": "Run tests by default deployment", "done": False},
-    {"id": 3, "name": "Deploy to ECS", "done": False},
+    {"id": 3, "name": "Deploy to ECS deployment", "done": False},
 ]
 
 @app.route('/health')
